4e6e29504b23324b483c6107dcb35a1f03b0ce8d
shuto-416 <hsjudo416@gmail.com>
2021-11-19 14:44:52 +0900

    adding navigation for only desktop but not implement Items yet

--- pages/Header.tsx
+++ pages/Header.tsx
@@ -1,11 +1,14 @@
 import {
     Box,
     Stack,
-    Heading,
+    Button,
     Flex,
     Text,
+    Link,
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
     Collapse,
-    Button,
     IconButton,
     useDisclosure,
     useColorModeValue,
@@ -54,9 +57,9 @@ const Header = () => {
                         MOVIECLIP
                     </Text>
 
-                    {/* <Flex display = {{base:'none', md:'flex'}} ml = {10} >
+                    <Flex display = {{base:'none', md:'flex'}} ml = {10} >
                         <DesktopNav />
-                    </Flex> */}
+                    </Flex>
                 </Flex>
 
                 <Stack
@@ -93,6 +96,57 @@ const Header = () => {
                 <MobileNav />
             </Collapse> */}
         </Box>
+    )
+}
+
+// Desktop size(一番でかいサイズ)の時のNav表示(title sign in sign up以外)
+const DesktopNav = () => {
+    const linkColor = useColorModeValue('gray.600', 'gray.200');
+    const linkHoverColor = useColorModeValue('gray.800', 'white');
+    const popoverContentBgColor = useColorModeValue('white', 'gray.800');
+
+    return(
+        <Stack direction = {'row'} spacing = {4}>
+            {NAV_ITEMS.map((navItem) => (
+                <Box key = {navItem.label}>
+                    <Popover trigger = {'hover'} placement = {'bottom-start'}>
+                        <PopoverTrigger>
+                            <Link
+                                p = {2} //padding
+                                href = {navItem.href ?? '#'} // link先なければ#
+                                fontSize = {'md'}
+                                fontWeight = {500}
+                                color = {linkColor}
+                                _hover = {{
+                                    textDecoration: 'none',
+                                    color: linkHoverColor,
+                                }}
+                            >
+                                {navItem.label}
+                            </Link>
+                        </PopoverTrigger>
+
+                        {navItem.children && (
+                            <PopoverContent
+                                border = {0}
+                                boxShadow = {'xl'} // xl: x-large
+                                bg = {popoverContentBgColor}
+                                p = {4}
+                                rounded = {'xl'}
+                                minW = {'sm'}
+                            >
+                                <Stack>
+                                    {/* {navItem.children.map((child) => (
+                                        <DesktopSubNav key={child.label} {...child}/>
+                                    ))} */}
+                                </Stack>
+
+                            </PopoverContent>
+                        )}
+                    </Popover>
+                </Box>
+            ))}
+        </Stack>
     )
 }
 
