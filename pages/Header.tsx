fb38bf2ee9f74cd0107aa780afabfdb375f7257b
shuto-416 <hsjudo416@gmail.com>
2021-11-19 16:07:26 +0900

    created header but design not good

--- pages/Header.tsx
+++ pages/Header.tsx
@@ -5,6 +5,7 @@ import {
     Flex,
     Text,
     Link,
+    Icon,
     Popover,
     PopoverTrigger,
     PopoverContent,
@@ -17,6 +18,8 @@ import {
 import { 
     HamburgerIcon,
     CloseIcon,
+    ChevronRightIcon,
+    ChevronDownIcon,
  } from "@chakra-ui/icons"
 
 const Header = () => {
@@ -50,7 +53,7 @@ const Header = () => {
 
                 <Flex flex = {{ base: 1 }} justify = {{ lg: 'center', md: 'start' }} >
                     <Text
-                        textAlign = {useBreakpointValue({ base: 'center', md: 'left' })} // ウィンドウサイズがmd以下ならcenter, md以上ならleft
+                        // textAlign = {useBreakpointValue({ base: 'center', md: 'left' })} // ウィンドウサイズがmd以下ならcenter, md以上ならleft
                         fontfamily = {'heading'}
                         color={useColorModeValue('gray.800', 'white')}
                     >
@@ -92,9 +95,9 @@ const Header = () => {
                 </Stack>
  
             </Flex>
-            {/* <Collapse in = {isOpen} animateOpacity>
+            <Collapse in = {isOpen} animateOpacity>
                 <MobileNav />
-            </Collapse> */}
+            </Collapse>
         </Box>
     )
 }
@@ -136,9 +139,9 @@ const DesktopNav = () => {
                                 minW = {'sm'}
                             >
                                 <Stack>
-                                    {/* {navItem.children.map((child) => (
+                                    {navItem.children.map((child) => (
                                         <DesktopSubNav key={child.label} {...child}/>
-                                    ))} */}
+                                    ))}
                                 </Stack>
 
                             </PopoverContent>
@@ -147,6 +150,110 @@ const DesktopNav = () => {
                 </Box>
             ))}
         </Stack>
+    )
+}
+
+const DesktopSubNav = ({ label, href, subLabel }: NavItem) => {
+    return(
+        <Link
+            href = {href}
+            role = {'group'}
+            display = {'block'}
+            p = {2}
+            rounded = {'md'}
+            _hover = {{ bg: useColorModeValue('pink.50', 'gray.900') }}
+        >
+            <Stack direction = {'row'} align = {'center'}>
+
+                <Box>
+                    <Text
+                        transition = {'all .3s ease'}
+                        _groupHover = {{ color: 'pink.400' }}
+                        fontWeight = {500}
+                    >
+                        {label}
+                    </Text>
+                    <Text fontSize = {'sm'}>{subLabel}</Text>
+                </Box>
+
+                <Flex
+                    transition = {'all .3s ease'}
+                    transform = {'translateX(-10px)'}
+                    opacity = {0}
+                    _groupHover = {{ opacity: '100%', transform: 'translateX(0)' }}
+                    justify = {'flex-end'}
+                    align = {'center'}
+                    flex = {1}
+                >
+                    <Icon color = {'pink.400'} h = {5} w = {5} as={ChevronRightIcon} />
+                </Flex>
+            </Stack>
+        </Link>
+    )
+}
+
+const MobileNav = () => {
+    return(
+        <Stack
+            bg = {useColorModeValue('white', 'gray.800')}
+            p = {4}
+            display = {{ md: 'none' }}
+        >
+            {NAV_ITEMS.map((navItem) => (
+                <MobileNavItem key={navItem.label} {...navItem} />
+            ))}
+        </Stack>
+    )
+}
+
+const MobileNavItem = ({ label, href, children }: NavItem) => {
+    const { isOpen, onToggle } = useDisclosure();
+
+    return(
+        <Stack spacing = {4} onClick={children && onToggle}>
+            <Flex
+                py = {2}
+                as = {Link}
+                href = { href ?? '#' }
+                justify = {'space-between'}
+                align = {'center'}
+                _hover = {{ textDecoration: 'none' }}
+            >
+                <Text
+                    fontWeight = {600}
+                    color = {useColorModeValue('gray.600', 'grey.200')}
+                >
+                    {label}
+                </Text>
+                {children && (
+                    <Icon
+                        as = {ChevronDownIcon}
+                        transition = {'all .25s ease-in-out'}
+                        transform = {isOpen ? 'rotate(180deg)' : ''}
+                        w = {6}
+                        h = {6}
+                    />
+                )}
+            </Flex>
+
+            <Collapse in = {isOpen} animateOpacity style = {{ marginTop: '0!important' }}>
+                <Stack
+                    mt = {2}
+                    pl = {4}
+                    borderLeft = {1}
+                    borderStyle = {'solid'}
+                    borderColor = {useColorModeValue('gray.200', 'gray.700')}
+                    align = {'start'}
+                >
+                    {children && children.map((child) => (
+                        <Link key={child.label} py={2} href={child.href}>
+                            {child.label}
+                        </Link>
+                    ))}
+                </Stack>
+            </Collapse>
+        </Stack>
+
     )
 }
 
