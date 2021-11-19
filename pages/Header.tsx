231d06961e85482229afce38434017fbdaa2ea53
shuto-416 <hsjudo416@gmail.com>
2021-11-19 14:47:35 +0900

    adding items

--- pages/Header.tsx
+++ pages/Header.tsx
@@ -150,4 +150,51 @@ const DesktopNav = () => {
     )
 }
 
+interface NavItem{
+    label: string;
+    subLabel?: string;
+    children?: Array<NavItem>;
+    href?: string;
+}
+
+const NAV_ITEMS: Array<NavItem> = [
+    {
+        label: 'Inspiration',
+        children: [
+          {
+            label: 'Explore Design Work',
+            subLabel: 'Trending Design to inspire you',
+            href: '#',
+          },
+          {
+            label: 'New & Noteworthy',
+            subLabel: 'Up-and-coming Designers',
+            href: '#',
+          },
+        ],
+      },
+      {
+        label: 'Find Work',
+        children: [
+          {
+            label: 'Job Board',
+            subLabel: 'Find your dream design job',
+            href: '#',
+          },
+          {
+            label: 'Freelance Projects',
+            subLabel: 'An exclusive list for contract work',
+            href: '#',
+          },
+        ],
+      },
+      {
+        label: 'Learn Design',
+        href: '#',
+      },
+      {
+        label: 'Hire Designers',
+        href: '#',
+      },
+];
 export default Header;
