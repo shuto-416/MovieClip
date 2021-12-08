d55cebf28f546059b3050ab4f2ffcc787a626728
shuto-416 <hsjudo416@gmail.com>
2021-12-08 14:42:51 +0900

    make hero of movieclip

--- pages/MovieClip/MovieClip.tsx
+++ pages/MovieClip/MovieClip.tsx
@@ -1,53 +1,69 @@
-import { Box, } from '@chakra-ui/react'
+import { 
+    Box,
+    Button,
+    Flex,
+    Stack,
+    VStack,
+    Text,
+    useBreakpointValue,
+} from '@chakra-ui/react'
 import * as React from 'react'
 import ClipsGrid from './ClipsGrid'
-// import { products } from './ClipItems'
+import { products } from './ClipItems'
 import ClipCard from './ClipCard'
-
-const products = [
-    {
-        id: "1",
-        name: "Product 1",
-        img: "https://via.placeholder.com/150",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        rating: 4.5,
-    },
-    {
-        id: "2",
-        name: "Product 2",
-        img: "https://via.placeholder.com/150",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        rating: 4.5,
-    },
-    {
-        id: "3",
-        name: "Product 3",
-        img: "https://via.placeholder.com/150",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        rating: 4.5,
-    },
-]
 
 const MovieClip = () => {
     return (
         <div>
-            {products.map((product) => {
-                <li>{product.description}, {product.name}</li>
-            })}
-        <Box
-            maxW={'7xl'}
-            mx={'auto'}
-            px={{ base: '4', md: '8', lg: '12' }}
-            py={{ base: '6', md: '8', lg: '12' }}
-        >
-            <ClipsGrid>
-                {products.map((product) => {
-                    return (
-                        <ClipCard product={product}/>
-                    )
-                })}
-            </ClipsGrid>
-        </Box>
+            <Flex 
+                w={'full'}
+                h={'80vh'}
+                backgroundImage={'https://source.unsplash.com/AtPWnYNDJnM'}
+                backgroundSize={'cover'}
+                backgroundPosition={'center center'}
+            >
+                <VStack
+                    w={'full'}
+                    justify={'center'}
+                    px={useBreakpointValue({ base: 4, md: 8 })}
+                    bgGradient={'linear(to-r, gray.900, transparent)'}
+                >
+                    <Stack maxW={'2xl'} align={'flex-start'} spacing={6}>
+                        <Text
+                            color={'white'}
+                            fontWeight={700}
+                            fontHeight={1.2}
+                            fontSize={useBreakpointValue({ base: '3xl', md: '4xl' })}
+                        >
+                            Movie Clip
+                        </Text>
+                        <Stack direction={'row'}>
+                            <Button
+                                bg={'purple.500'}
+                                rounded={'full'}
+                                color={'white'}
+                                _hover={{ bg: 'purple.600' }}
+                            >
+                                Watch Now
+                            </Button>
+                        </Stack>   
+                    </Stack>
+                </VStack>
+            </Flex>
+            <Box
+                maxW={'7xl'}
+                mx={'auto'}
+                px={{ base: '4', md: '8', lg: '12' }}
+                py={{ base: '6', md: '8', lg: '12' }}
+            >
+                <ClipsGrid>
+                    {products.map((product) => {
+                        return (
+                            <ClipCard product={product}/>
+                        )
+                    })}
+                </ClipsGrid>
+            </Box>
         </div>
     )
 }
